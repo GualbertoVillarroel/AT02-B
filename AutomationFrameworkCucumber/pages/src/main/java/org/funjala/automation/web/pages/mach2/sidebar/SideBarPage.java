83b8c987aa91646fdf60ccc79f499e405b8455bc
Jorge <jorge.forero@fundacion-jala.org>
2017-01-25 17:35:49 -0400

    delete all boards

--- AutomationFrameworkCucumber/pages/src/main/java/org/funjala/automation/web/pages/mach2/sidebar/SideBarPage.java
+++ AutomationFrameworkCucumber/pages/src/main/java/org/funjala/automation/web/pages/mach2/sidebar/SideBarPage.java
@@ -1,9 +1,12 @@
 package org.funjala.automation.web.pages.mach2.sidebar;
 
 import org.funjala.automation.web.model.mach2.sidebar.SideBarModel;
+import org.funjala.automation.web.pages.mach2.board.BoardOptions;
 import org.funjala.automation.web.pages.mach2.dashboard.MyDashboard;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -25,6 +28,9 @@ public class SideBarPage {
   @FindBy(css = SideBarModel.myDashboardButton)
   private WebElement myDashboardButton;
 
+  @FindBy(xpath = "//div[@class='menu ui-sortable']")
+  private WebElement boardsList;
+
 
   public SideBarPage(WebDriver driver) {
     this.driver = driver;
@@ -39,6 +45,20 @@ public class SideBarPage {
     return new MyDashboard(driver);
   }
 
+  public BoardOptions rightClickBoardCard(final WebElement board) {
+    Actions action = new Actions(driver);
+    action.contextClick(board).build().perform();
+    return new BoardOptions(driver);
+  }
 
 
+  public void deleteAllBoards() {
+    wait.until(ExpectedConditions.visibilityOf(boardsList));
+    boardsList.findElements(By.tagName("a")).forEach(element -> {
+      wait.until(ExpectedConditions.visibilityOf(element));
+      wait.until(ExpectedConditions.elementToBeClickable(element));
+      BoardOptions boardOption = rightClickBoardCard(element);
+      boardOption.deleteBoardMach();
+    });
+  }
 }
