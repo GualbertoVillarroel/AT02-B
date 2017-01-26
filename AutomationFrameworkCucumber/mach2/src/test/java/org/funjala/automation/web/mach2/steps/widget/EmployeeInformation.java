2a2b54bc60293bbdc12aed0d5f0aa9b1f8c0dcc3
GualbertoVillarroel <gualy.vc7@gmail.com>
2017-01-26 17:25:35 -0400

    TableEnInForManager feature and step

--- AutomationFrameworkCucumber/mach2/src/test/java/org/funjala/automation/web/mach2/steps/widget/EmployeeInformation.java
+++ AutomationFrameworkCucumber/mach2/src/test/java/org/funjala/automation/web/mach2/steps/widget/EmployeeInformation.java
@@ -25,107 +25,106 @@ import static org.testng.Assert.assertEquals;
  * Created by Angelica Rodriguez on 1/21/2017.
  */
 public class EmployeeInformation {
-    WebDriver driver;
-    TopMenuPage topMenuPage;
-    LoginPage loginPage;
-    WidgetPage widget;
+  WebDriver driver;
+  TopMenuPage topMenuPage;
+  LoginPage loginPage;
+  WidgetPage widget;
 
-    @Given("^I am on Mach2 Webpage$")
-    public void iAmOnMachWebpage() throws IOException {
-        driver = Driver.getDriver().openBrowser(Driver.Mach2);
-        loginPage = new LoginPage(driver);
-    }
+  @Given("^I am on Mach2 Webpage$")
+  public void iAmOnMachWebpage() throws IOException {
+    driver = Driver.getDriver().openBrowser(Driver.Mach2);
+    loginPage = new LoginPage(driver);
+  }
 
-    @And("^I fill username or Mail textfield as \"([^\"]*)\"$")
-    public void iFillUsernameOrMailTextfieldAs(String userName) {
-        loginPage.setUsernameTextField(userName);
-    }
+  @And("^I fill username or Mail textfield as \"([^\"]*)\"$")
+  public void iFillUsernameOrMailTextfieldAs(String userName) {
+    loginPage.setUsernameTextField(userName);
+  }
 
-    @And("^I fill Password textfield as \"([^\"]*)\" and press Submit button$")
-    public void iFillPasswordTextfieldAsAndPressSubmitButton(String password) {
-        loginPage.setPasswordTextField(password);
-        topMenuPage = loginPage.clickNextButton();
-    }
+  @And("^I fill Password textfield as \"([^\"]*)\" and press Submit button$")
+  public void iFillPasswordTextfieldAsAndPressSubmitButton(String password) {
+    loginPage.setPasswordTextField(password);
+    topMenuPage = loginPage.clickNextButton();
+  }
 
-    @Given("^I have board created$")
-    public void iHaveABoardCreated() {
-        topMenuPage.addNewBoard();
-    }
+  @Given("^I have board created$")
+  public void iHaveABoardCreated() {
+    topMenuPage.addNewBoard();
+  }
 
-    @And("^I click on Widget Button$")
-    public void iClickOnWidgetButton() {
-        widget = topMenuPage.addNewWidget();
-    }
+  @And("^I click on Widget Button$")
+  public void iClickOnWidgetButton() {
+    widget = topMenuPage.addNewWidget();
+  }
 
-    @When("^I click on \"([^\"]*)\" Button on Widget displayed$")
-    public void iClickOnTableButtonOnWidgetDisplayed(String widgetType) {
-        widget.addWidget(widgetType);
-    }
+  @When("^I click on \"([^\"]*)\" Button on Widget displayed$")
+  public void iClickOnTableButtonOnWidgetDisplayed(String widgetType) {
+    widget.addWidget(widgetType);
+  }
 
-    @And("^I click on \"([^\"]*)\" services$")
-    public void iClickOnOpenERPService(String service) {
-        widget.clickOnService(service);
-    }
+  @And("^I click on \"([^\"]*)\" services$")
+  public void iClickOnOpenERPService(String service) {
+    widget.clickOnService(service);
+  }
 
-    @And("^I select an Option of OpenERP$")
-    public void iSelectAnOptionOfOpenERP() {
-        widget.selectErpOption();
-    }
-
-
-
-    @And("^In the option Divisions I select \"([^\"]*)\"$")
-    public void iFillDivisionNameOnTextfieldAs(String divisionName) {
-        widget.setDivisionName(divisionName);
-    }
+  @And("^I select an Option of OpenERP$")
+  public void iSelectAnOptionOfOpenERP() {
+    widget.selectErpOption();
+  }
 
 
-    @And("^In the option Manager \"([^\"]*)\"$")
-    public void iFillManagerNameOnTextfieldAs(String managerName) {
-        widget.setManagerName(managerName);
-    }
+  @And("^In the option Divisions I select \"([^\"]*)\"$")
+  public void iFillDivisionNameOnTextfieldAs(String divisionName) {
+    widget.setDivisionName(divisionName);
+  }
 
-    @And("^I click on save Button$")
-    public void iClickOnSaveButton() {
-        widget.clickSaveButton();
-    }
 
-    @Then("^I have a table widget with \"([^\"]*)\" Division and \"([^\"]*)\" Manager filled$")
-    public void iHaveATableWidgetWithFilled(String division, String managerName) throws IOException, InterruptedException {
-        int actualResult = widget.verifyCant(managerName);
-        System.out.println(">>>>>>>>>>>>>>>>>>>");
-        System.out.println(actualResult);
-        System.out.println(">>>>>>>>>>>>>>>>>>>");
-        SideBarPage sideBarPage = topMenuPage.goToSidebar();
+  @And("^In the option Manager \"([^\"]*)\"$")
+  public void iFillManagerNameOnTextfieldAs(String managerName) {
+    widget.setManagerName(managerName);
+  }
+
+  @And("^I click on save Button$")
+  public void iClickOnSaveButton() {
+    widget.clickSaveButton();
+  }
+
+  @Then("^I have a table widget with \"([^\"]*)\" Division and \"([^\"]*)\" Manager filled$")
+  public void iHaveATableWidgetWithFilled(String division, String managerName) throws IOException, InterruptedException {
+    int actualResult = widget.verifyCant(managerName);
+    System.out.println(">>>>>>>>>>>>>>>>>>>");
+    System.out.println(actualResult);
+    System.out.println(">>>>>>>>>>>>>>>>>>>");
+    SideBarPage sideBarPage = topMenuPage.goToSidebar();
 //        sideBarPage.deleteAllBoards();
 
-        //Login OPEN ERP
+    //Login OPEN ERP
 
-        driver = Driver.getDriver().openBrowser(Driver.OpenERP);
-        OELoginPage loginERP = new OELoginPage(driver);
-        loginERP.setUserName("jose6");
-        loginERP.setPassword("jose6");
-        OEHomePage homeERP = loginERP.clickBtnSubmit();
+    driver = Driver.getDriver().openBrowser(Driver.OpenERP);
+    OELoginPage loginERP = new OELoginPage(driver);
+    loginERP.setUserName("jose6");
+    loginERP.setPassword("jose6");
+    OEHomePage homeERP = loginERP.clickBtnSubmit();
 
-        //Go to Human Resources
+    //Go to Human Resources
 
-        homeERP.clickHumanResources();
-        OESearch searchERP = homeERP.oeSearch();
+    homeERP.clickHumanResources();
+    OESearch searchERP = homeERP.oeSearch();
 
-        //Go to Search
-        searchERP.clickSearchArrow();
-        searchERP.clickAdvancedSearch();
-        searchERP.foundAndClickAdvancedFilterOptions("manager", "is equal to", managerName);
-        searchERP.clickAddAConditionButton();
-        searchERP.foundAndClickAdvancedFilterOptions("division", "is equal to", division);
-        searchERP.clickApplySearch();
-        searchERP.clickSwitchList();
-        searchERP.clickNumberElement();
-        searchERP.clickQuantityButton();
-        searchERP.clickUnlimitedOption();
-        List<WebElement> listManager = searchERP.listOfAllElements();
+    //Go to Search
+    searchERP.clickSearchArrow();
+    searchERP.clickAdvancedSearch();
+    searchERP.foundAndClickAdvancedFilterOptions("manager", "is equal to", managerName);
+    searchERP.clickAddAConditionButton();
+    searchERP.foundAndClickAdvancedFilterOptions("division", "is equal to", division);
+    searchERP.clickApplySearch();
+    searchERP.clickSwitchList();
+    searchERP.clickNumberElement();
+    searchERP.clickQuantityButton();
+    searchERP.clickUnlimitedOption();
+    List<WebElement> listManager = searchERP.listOfAllElements();
 
-        assertEquals(listManager.size(), actualResult);
-    }
+    assertEquals(listManager.size(), actualResult);
+  }
 
 }
