58b3aadd55c6c5a949f6c98e23b3258acdab042a
Sergio <sergio.landa@fundacion-jala.org>
2017-01-27 08:31:08 +0000

    refactor

--- AutomationFrameworkCucumber/mach2/src/test/java/org/funjala/automation/web/mach2/steps/widget/TablePerInForDepartmentsSteps.java
+++ AutomationFrameworkCucumber/mach2/src/test/java/org/funjala/automation/web/mach2/steps/widget/TablePerInForDepartmentsSteps.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.funjala.automation.web.common.drivers.Driver;
+import org.funjala.automation.web.common.utilities.Log;
 import org.funjala.automation.web.model.erp.search.ModelSearch;
 import org.funjala.automation.web.pages.erp.home.OEHomePage;
 import org.funjala.automation.web.pages.erp.login.OELoginPage;
@@ -40,16 +41,19 @@ public class TablePerInForDepartmentsSteps {
   OESearch oeSearch;
   List<ModelSearch> listModelSearch = new ArrayList<>();
   ModelSearch modelSearch;
+  Log log = Log.getInstance();
   int x = 0;
 
   @Given("^I go to Mach2 web page$")
   public void iGoToMachWebPage() throws Throwable {
+    log.info("Step", "I go to Mach2 web page", "Go to Mach2 web page");
     driver = Driver.getDriver().openBrowser(Driver.Mach2);
     loginPage = new LoginPage(driver);
   }
 
   @And("^I set user and password$")
   public void iSetUserAndPassword() {
+    log.info("Step", "I set a username and a password ","Set a valid user name");
     loginPage.setUsernameTextField("at02");
     loginPage.setPasswordTextField("Automation123");
     topMenuPage = loginPage.clickNextButton();
@@ -57,34 +61,40 @@ public class TablePerInForDepartmentsSteps {
 
   @Given("^I have a board$")
   public void iHaveABoard() {
+    log.info("Step", "I have a board ","create a board");
     topMenuPage.addNewBoard();
   }
 
   @And("^I click over Widget Button$")
   public void iClickOverWidgetButton() {
+    log.info("Step", "I click over Widget Button","create a widget");
     widget = topMenuPage.addNewWidget();
   }
 
   @When("^I click on Table button on the Widget$")
   public void iClickOnTableButtonOnTheWidget() {
+    log.info("Step", "I click on Table button on the Widget","select a table");
     widget.addWidget("Table");
 
   }
 
   @And("^I click on Open ERP service$")
   public void iClickOnOpenERPService() {
+    log.info("Step", "I click on Open ERP service","select a service");
     widget.clickOnService("Open ERP");
 
   }
 
   @And("^I select Option of Open ERP$")
   public void iSelectOptionOfOpenERP() {
+    log.info("Step", "I select Option of Open ERP","select a OPENERP");
     widget.selectErpOption();
 
   }
 
   @And("^I set department name with \"([^\"]*)\"$")
   public void iSetDepartmentNameWith(String arg0) {
+    log.info("Step", "I set department name with a name","select a Security test");
     widget.clickOnAdvanceConfiguration();
     widget.setDepartmentName("Security test");
 
@@ -92,12 +102,14 @@ public class TablePerInForDepartmentsSteps {
 
   @And("^I save that option with the department \"([^\"]*)\" selected$")
   public void iSaveThatOption(String department) {
+    log.info("Step", "I save that option with the department","save option");
     widget.clickSaveButton();
     x = widget.verifyCantDepartment(department);
   }
 
   @Then("^I have a table with employees filtered for a \"([^\"]*)\"$")
   public void iHaveATableWithEmployeesFilteredForA(String s) throws InterruptedException, IOException {
+    log.info("Step", "I have a table with employees filtered","expected result");
     MyDashboard myDashboard = new MyDashboard(driver);
     myDashboard.deleteBoard();
 
